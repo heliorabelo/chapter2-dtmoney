0308d095521837d14303e50b1b0e79d37bdc4d87
Helio Souza <heliobrunorabelo@gmail.com>
2021-12-06 12:18:44 -0300

    Criaçao de nova transaçao, listagem e formataçao de informaçao

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 import { App } from "./App";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freela",
+          type: "deposit",
+          category: "Dev",
+          amount: 50000,
+          createdAt: new Date("2021-12-20 11:00:00"),
+        },
+        {
+          id: 2,
+          title: "Mercado",
+          type: "withdraw",
+          category: "Casa",
+          amount: 2000,
+          createdAt: new Date("2021-12-22 09:30:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      console.log(data);
+
+      console.log(this.schema.all("transaction"));
+      return schema.create("transaction", data);
     });
   },
 });
